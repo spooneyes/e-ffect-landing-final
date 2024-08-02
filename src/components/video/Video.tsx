4cbb602b0fd1663fd535d468e410b6a291415a24
georges <georges.legentil@gmail.com>
2024-08-02 15:37:45 +0200

    change video to reloative

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,6 +1,6 @@
 export function Video() {
   return (
-    <div className="hidden h-full md:flex">
+    <div className="relative hidden h-full overflow-hidden md:flex">
       <video autoPlay loop muted>
         <source src="/video.mov" type="video/mp4" />
         Your browser does not support the video tag.
