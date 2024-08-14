744772fbb4fd0949bc1af318d7a99641ca279a20
georges <georges.legentil@gmail.com>
2024-08-14 17:51:53 +0200

    change color ai pres

--- src/components/features/grid/ModalContent/ProductContent.tsx
+++ src/components/features/grid/ModalContent/ProductContent.tsx
@@ -7,13 +7,13 @@ export const BouncyCardsFeatures = () => {
       <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-8">
         <h2 className="max-w-lg text-4xl  font-bold text-neutral-300 md:text-5xl">
           Meet Your Ultimate AI
-          <span className="text-slate-400"> Guitar Assistant</span>
+          <span className="text-slate-500"> Guitar Assistant</span>
         </h2>
       </div>
       <div className="mb-4 grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-4">
           <CardTitle>Sound Like the Legends</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-violet-600 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-indigo-50">
               Replicate iconic guitar tones, like Eddie Van Halen's "Beat It"
               solo. <br /> Explore styles from legends like Jimi Hendrix and
@@ -23,7 +23,7 @@ export const BouncyCardsFeatures = () => {
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-8">
           <CardTitle>Create Unique Sounds</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-sky-400 to-blue-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-orange-50">
               Mimic instruments like piano, flute, and violin. <br /> Experience
               effects like underwater ambiance, empty concert halls, and open
@@ -44,7 +44,7 @@ export const BouncyCardsFeatures = () => {
         </BounceCard>
         <BounceCard className="col-span-12 md:col-span-4">
           <CardTitle>Expand Your Musical Imagination</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-pink-400 to-red-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-red-400 to-red-500 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
             <span className="block text-center font-semibold text-red-50">
               Enjoy new sounds and redefine your musical journey. <br />
               Perfect for all skill levels, whether you're just starting out or
@@ -67,7 +67,7 @@ const BounceCard = ({
   return (
     <motion.div
       whileHover={{ scale: 0.95, rotate: "-1deg" }}
-      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-700 p-8 ${className}`}
+      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-zinc-800 p-8 ${className}`}
     >
       {children}
     </motion.div>
