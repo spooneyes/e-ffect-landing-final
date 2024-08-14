99610ea7a180fac95d0b0749d7d99f0eb918b0af
georges <georges.legentil@gmail.com>
2024-08-14 18:06:26 +0200

    Meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
       <Head />
+      <meta
+        name="description"
+        content="We are happy to introduce our new product : W4P. Discover it now !"
+      />
+
+      <title>E-ffect | W4P</title>
       <body className=" bg-zinc-950 text-zinc-200 selection:bg-zinc-600">
         <Main />
         <NextScript />
