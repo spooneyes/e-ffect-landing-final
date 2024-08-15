7a55b18da65091c11f32e8f74a5b7a489ba8f849
georges <georges.legentil@gmail.com>
2024-08-15 11:18:52 +0200

    remove video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         {/* <Customers /> */}
         {/* <Stats /> */}
         {/* <Pricing /> */}
-        <Video />
+        {/* <Video /> */}
         <CTA />
         <Footer />
       </main>
