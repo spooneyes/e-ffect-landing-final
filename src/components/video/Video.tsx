c5a07e6d936c92617d86b68d00cd7519e4bc1459
georges <georges.legentil@gmail.com>
2024-08-15 02:38:20 +0200

    video adjustement

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,7 +1,7 @@
 export function Video() {
   return (
     <div className="relative hidden h-full overflow-hidden md:flex">
-      <video autoPlay loop muted>
+      <video className="h-full w-full object-cover" autoPlay loop muted>
         <source src="/video.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
