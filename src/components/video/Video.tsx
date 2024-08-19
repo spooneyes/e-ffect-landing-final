9d8586a5d22982b3d92be5cb1726567aebb38f4f
georges <georges.legentil@gmail.com>
2024-08-19 16:38:45 +0200

    youtube embed

--- src/components/video/Video.tsx
+++ src/components/video/Video.tsx
@@ -1,10 +1,15 @@
 export function Video() {
   return (
     <div className="relative hidden h-full overflow-hidden md:flex">
-      <video className="relative" autoPlay loop muted>
-        <source src="/video.mov" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <iframe
+        style={{
+          width: "100%",
+          height: "1000px",
+        }}
+        src="https://www.youtube.com/embed/Q4vhVNA_iso?si=uh1hBoBh1d5vTqhG&controls=0&autoplay=1&mute=1&loop=1&playlist=Q4vhVNA_iso&hd=1"
+        title="YouTube video player"
+        allowFullScreen={false}
+      ></iframe>
     </div>
   );
 }
